6e74dd0848bfae4e731de4023ce611c77c3b7a4c
Léa DUKAEZ <lea.dukaez@gmail.com>
2020-06-14 22:12:11 +0200

    password reset option added

--- Insanity/Constants.swift
+++ Insanity/Constants.swift
@@ -15,6 +15,7 @@ struct K {
     static let segueHomeToTest = "goToTest"
     static let segueHomeToAccount = "goToAccount"
     static let segueWelcomeToHome = "welcomeToHomeloggedIn"
+    static let segueToReset = "goToReset"
     
     static let avatarImages = ["avatar1", "avatar2", "avatar3", "avatar4", "avatar5", "avatar6", "avatar7", "avatar8", "avatar9", "avatar10", "avatar11", "avatar12", "avatar13", "avatar14", "avatar15", "avatar16", "avatar17", "avatar18"]
     static let reuseAvatarIdentifier = "reuseAvatarCell"
---
+++ Insanity/Controller/NewPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewPasswordViewController.swift
+//  Insanity
+//
+//  Created by Léa on 14/06/2020.
+//  Copyright © 2020 Lea Dukaez. All rights reserved.
+//
+
+import UIKit
+
+class NewPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
